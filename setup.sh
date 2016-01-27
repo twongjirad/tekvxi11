0d7b2dede622124d63d62798ea3c0bf8a9572d99
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2016-01-27 14:05:54 -0800

    updated path variables

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #test commit
 export HODODAQ_HOME=/home/twongjirad/working/HodoDAQ
+export TEKVXI11_DIR=/home/twongjirad/working/tekvxi11/
+
 export HODODAQ_INCDIR=${HODODAQ_HOME}/include
 export HODODAQ_LIBDIR=${HODODAQ_HOME}/lib
-export TEKVXI11_DIR=/home/twongjirad/working/tekvxi11/
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TEKVXI11_DIR}:${TEKVXI11_DIR}/vxi11_1.10
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TEKVXI11_DIR}/hododaq:${HODODAQ_LIBDIR}:${HODODAQ_HOME}
